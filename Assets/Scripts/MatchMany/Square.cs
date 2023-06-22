07208fed35e9a80c9ecd880bb3dc976b77384948
Bartu İnce <sonic22mario@hotmail.com>
2023-06-22 07:53:41 +0300

    start match scene

--- Assets/Scripts/MatchMany/Square.cs
+++ Assets/Scripts/MatchMany/Square.cs
@@ -6,10 +6,20 @@ public class Square : MonoBehaviour
 {
     [SerializeField] private Vector2 itemCenter;
     [SerializeField] private Transform itemObject;
+    [SerializeField] private SpriteRenderer itemSprite;
 
     public SquarePosition coordinates;
-    
     public ItemType m_ItemType;
+
+    public bool IsCompleted;
+
+    [Header("Item Type Sprites")]
+    [SerializeField] private Sprite redSprite;
+    [SerializeField] private Sprite blueSprite;
+    [SerializeField] private Sprite greenSprite;
+    [SerializeField] private Sprite colorlessSprite;
+    [SerializeField] private Sprite completedSprite;
+    
 
 
     public void SetItemType(ItemType type)
@@ -18,22 +28,36 @@ public class Square : MonoBehaviour
     }
 
 
-    public void SwapTo(Vector2 targetPos)
-    {
-        
-    }
-    
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetItemSprite();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetItemSprite()
     {
+        itemSprite.sprite = GetTypeSprite(m_ItemType);
+    }
+    
+    private Sprite GetTypeSprite(ItemType type)
+    {
+        if (IsCompleted)
+            return completedSprite;
         
+        switch (type)
+        {
+            case ItemType.Red:
+                return redSprite;
+            case ItemType.Blue:
+                return blueSprite;
+            case ItemType.Green:
+                return greenSprite;
+            case ItemType.Colorless:
+                return colorlessSprite;
+            default:
+                Debug.LogError("How'd you make a TYPELESS square?!");
+                return completedSprite;
+        }
     }
 }
 
